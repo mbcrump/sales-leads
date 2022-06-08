c3a311c3cda8392bb3947fe38ab646a05f9aabc1
Michael Crump <mbcrump29@gmail.com>
2022-06-08 10:18:27 -0700

    Add project files.

---
+++ sales-leads/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sales_leads.Models;
+using Vereyon.Web;
+using Vonage;
+using Vonage.Request;
+
+namespace sales_leads.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Lead lead)
+        {
+            string name = lead.Name;
+            string phone = lead.Phone;
+            string message = lead.Message;
+
+            var credentials = Credentials.FromApiKeyAndSecret(
+            Domain.Credentials.APIKey,
+            Domain.Credentials.APISecret
+            );
+
+            var VonageClient = new VonageClient(credentials);
+
+            var response = VonageClient.SmsClient.SendAnSms(new Vonage.Messaging.SendSmsRequest()
+            {
+                To = "14253820339",
+                From = "18335787280",
+                Text = $"New lead acquired:\nName:{name}\nPhone:{phone}\nMessage:{message}"
+            });
+
+            if (response != null && Convert.ToInt32(response.MessageCount) > 0 && response.Messages[0].StatusCode.ToString() == "Success")
+            {
+                lead.Result = "Success";
+            }
+            else
+            {
+                lead.Result = "Failure";
+            }
+            
+            return View(lead);
+        }
+    }
+}
---
+++ sales-leads/Domain/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace sales_leads.Domain
+{
+    public class Credentials
+    {
+        public static string APIKey => "d662fefa";
+
+        public static string APISecret => "dsasP1vfLflSH96U";
+    }
+
+}
